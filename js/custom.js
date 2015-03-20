1fa85fc195fe839967c6bc7cbc9e07566cc6b4dc
KidneyForDinner <ptforness@gmail.com>
2015-03-19 21:30:47 -0500

    V2.0
    New splash page, profile picture and navbar.

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function (readyfreddy) {
+    
+    "use strict";
+    $(".navbar").ready(function (movedown) {
+        $("#ihatejquery").animate({ 'marginTop': '+57px' }, 1000);
+    });
+    
+    $("liberum:child").ready(function (fn) {
+        $("#ilovejquery").fadeIn(1600);
+    });
+    
+        $("profilepicdiv:child").ready(function (fn) {
+        $("#thirdtimesthecharm").fadeIn(1300);
+    });
+    
+});
